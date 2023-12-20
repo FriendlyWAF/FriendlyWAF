9e4d20ed63aa66b1a10b14eb109a756d6bf9367a
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2023-12-20 22:52:34 +0100

    Update Installer-FriendlyWAF.sh

--- Installer-FriendlyWAF.sh
+++ Installer-FriendlyWAF.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # this command give the script sudo perms
 if [ `id -u` -ne 0 ]; then
-	echo Need sudo
-	exit 1
+        echo Need sudo
+        exit 1
 fi
 
 set -v
-sleep 5
+sleep 25
 clear
 
 ######################################################################################
@@ -30,117 +30,203 @@ sudo sudo apt-get install speedtest
 sleep 2
 wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh
 sleep 3
-# logging the connections
-iptables -D INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+ufw allow 443/tcp
+sleep 1
+ufw allow 25565/tcp
+sleep 1
+ufw allow 81/tcp
+sleep 1
+ufw allow 19999/tcp
+sleep 1
+ufw allow 80/tcp
 sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -D INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
+ufw reload
 sleep 2
-iptables -D INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -j DROP
+cd /etc/ufw/
 sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -D INPUT -p tcp --dport 81 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
+
+# Get a list of active network interfaces
+active_interfaces=$(ip link | grep 'state UP' | cut -d ':' -f 2)
+
+# Loop through each interface and select the first active one
+uplink=""
+for interface in $active_interfaces; do
+    uplink=$interface
+    break
+done
+
+if [ -z "$uplink" ]; then
+    echo "No active network interface found."
+else
+echo "
+#
+# rules.before
+#
+# Rules that should be run before the ufw command line added rules. Custom
+# rules should be added to one of these chains:
+#   ufw-before-input
+#   ufw-before-output
+#   ufw-before-forward
+#
+
+# Don't delete these required lines, otherwise there will be errors
+*filter
+:ufw-before-input - [0:0]
+:ufw-before-output - [0:0]
+:ufw-before-forward - [0:0]
+:ufw-not-local - [0:0]
+# End required lines
+
+# Rate limit ICMP echo requests to 20 per hour
+-A ufw-before-input -p icmp --icmp-type echo-request -m limit --limit 20/hour -j ACCEPT
+
+# ----- 2 concurrent connections per ip -----
+# TCP
+-A ufw-before-input -p tcp --syn --dport 22 -m connlimit --connlimit-above 2 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 22 -m connlimit --connlimit-above 2 -j DROP
+
+# ----- 5 connections per 1 hour per ip -----
+# TCP
+-A ufw-before-input -p tcp --dport 22 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p tcp --dport 22 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 22 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p udp --dport 22 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+
+# ----- 2 concurrent connections per ip -----
+# TCP
+-A ufw-before-input -p tcp --syn --dport 443 -m connlimit --connlimit-above 2 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 443 -m connlimit --connlimit-above 2 -j DROP
+
+# ----- 5 connections per 1 hour per ip -----
+# TCP
+-A ufw-before-input -p tcp --dport 443 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p tcp --dport 443 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 443 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p udp --dport 443 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+
+# ----- 2 concurrent connections per ip -----
+# TCP
+-A ufw-before-input -p tcp --syn --dport 80 -m connlimit --connlimit-above 2 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 80 -m connlimit --connlimit-above 2 -j DROP
+
+# ----- 5 connections per 1 hour per ip -----
+# TCP
+-A ufw-before-input -p tcp --dport 80 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p tcp --dport 80 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 80 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p udp --dport 80 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+
+# ----- 2 concurrent connections per ip -----
+# TCP
+-A ufw-before-input -p tcp --syn --dport 81 -m connlimit --connlimit-above 2 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 81 -m connlimit --connlimit-above 2 -j DROP
+
+# ----- 5 connections per 1 hour per ip -----
+# TCP
+-A ufw-before-input -p tcp --dport 81 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p tcp --dport 81 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 81 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p udp --dport 81 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+
+# ----- 2 concurrent connections per ip -----
+# TCP
+-A ufw-before-input -p tcp --syn --dport 19999 -m connlimit --connlimit-above 2 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 19999 -m connlimit --connlimit-above 2 -j DROP
+
+# ----- 5 connections per 1 hour per ip -----
+# TCP
+-A ufw-before-input -p tcp --dport 19999 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p tcp --dport 19999 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 19999 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p udp --dport 19999 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+
+# ----- 2 concurrent connections per ip -----
+# TCP
+-A ufw-before-input -p tcp --syn --dport 53 -m connlimit --connlimit-above 2 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 53 -m connlimit --connlimit-above 2 -j DROP
+
+# ----- 4 connections per 1 hour per ip -----
+# TCP
+-A ufw-before-input -p tcp --dport 53 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p tcp --dport 53 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 4 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 53 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p udp --dport 53 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 4 -j DROP
+
+# ----- 2 concurrent connections per ip -----
+# TCP
+-A ufw-before-input -p tcp --syn --dport 25565 -m connlimit --connlimit-above 2 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 25565 -m connlimit --connlimit-above 2 -j DROP
+
+# ----- 5 connections per 1 hour per ip -----
+# TCP
+-A ufw-before-input -p tcp --dport 25565 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p tcp --dport 25565 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+# UDP
+-A ufw-before-input -p udp --dport 25565 -i $uplink -m state --state NEW -m recent --set
+-A ufw-before-input -p udp --dport 25565 -i $uplink -m state --state NEW -m recent --update --seconds 3600 --hitcount 5 -j DROP
+
+# allow all on loopback
+-A ufw-before-input -i lo -j ACCEPT
+-A ufw-before-output -o lo -j ACCEPT
+
+# quickly process packets for which we already have a connection
+-A ufw-before-input -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+-A ufw-before-output -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+-A ufw-before-forward -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# drop INVALID packets (logs these in loglevel medium and higher)
+-A ufw-before-input -m conntrack --ctstate INVALID -j ufw-logging-deny
+-A ufw-before-input -m conntrack --ctstate INVALID -j DROP
+
+# allow dhcp client to work
+-A ufw-before-input -p udp --sport 67 --dport 68 -j ACCEPT
+
+#
+# ufw-not-local
+#
+-A ufw-before-input -j ufw-not-local
+
+# if LOCAL, RETURN
+-A ufw-not-local -m addrtype --dst-type LOCAL -j RETURN
+
+# if MULTICAST, RETURN
+-A ufw-not-local -m addrtype --dst-type MULTICAST -j RETURN
+
+# if BROADCAST, RETURN
+-A ufw-not-local -m addrtype --dst-type BROADCAST -j RETURN
+
+# all other non-local packets are dropped
+-A ufw-not-local -m limit --limit 3/min --limit-burst 10 -j ufw-logging-deny
+-A ufw-not-local -j DROP
+
+# allow MULTICAST mDNS for service discovery (be sure the MULTICAST line above
+# is uncommented)
+-A ufw-before-input -p udp -d 224.0.0.251 --dport 5353 -j ACCEPT
+
+# allow MULTICAST UPnP for service discovery (be sure the MULTICAST line above
+# is uncommented)
+-A ufw-before-input -p udp -d 239.255.255.250 --dport 1900 -j ACCEPT
+
+# don't delete the 'COMMIT' line or these rules won't be processed
+COMMIT
+" > before.rules
 sleep 2
-iptables -D INPUT -p tcp --dport 81 -m conntrack --ctstate NEW -j DROP
+fi
 sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -D INPUT -p tcp --dport 53 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -D INPUT -p tcp --dport 53 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# Icmp blocking 
-iptables -D INPUT -p icmp -m icmp --icmp-type address-mask-request -j DROP
-sleep 2
-iptables -D INPUT -p icmp -m icmp --icmp-type timestamp-request -j DROP
-sleep 2
-iptables -D INPUT -p icmp -m icmp --icmp-type 1 -m limit --limit 10/min -j ACCEPT 
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -D INPUT -p tcp --dport 80 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -D INPUT -p tcp --dport 80 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -D INPUT -p tcp --dport 10000 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -D INPUT -p tcp --dport 10000 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -D INPUT -p tcp --dport 19999 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -D INPUT -p tcp --dport 19999 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -D INPUT -p tcp --dport 443 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -D INPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -D INPUT -p tcp --dport 25565 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -D INPUT -p tcp --dport 25565 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# Reconfigure your Iptables
-sleep 4
-# logging the connections
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -A INPUT -p tcp --dport 81 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -A INPUT -p tcp --dport 81 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -A INPUT -p tcp --dport 53 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -A INPUT -p tcp --dport 53 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# Icmp blocking 
-iptables -A INPUT -p icmp -m icmp --icmp-type address-mask-request -j DROP
-sleep 2
-iptables -A INPUT -p icmp -m icmp --icmp-type timestamp-request -j DROP
-sleep 2
-iptables -A INPUT -p icmp -m icmp --icmp-type 2 -m limit --limit 2/hour -j ACCEPT 
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -A INPUT -p tcp --dport 10000 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -A INPUT -p tcp --dport 10000 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -A INPUT -p tcp --dport 19999 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -A INPUT -p tcp --dport 19999 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# These rules continue to accept new connections as long as they don’t exceed the limit of 3 connections per hour from each IP address.
-iptables -A INPUT -p tcp --dport 25565 -m conntrack --ctstate NEW -m limit --limit 3/hour --limit-burst 3 -j ACCEPT
-sleep 2
-iptables -A INPUT -p tcp --dport 25565 -m conntrack --ctstate NEW -j DROP
-sleep 2
-# Save Iptables Commands
-sudo /sbin/iptables-save > /etc/iptables/rules.v4
-sleep 2
-# installing Web Panel
-cd /var/www/html/
-sleep 2
-wget https://mirror.friendlywaf.com/Scripts-CE/WebPanel.zip
-sleep 2
-sudo unzip WebPanel.zip
+ufw reload
 sleep 2
 cd /root/
 # Installing Snort ++ for IPS/IDS
@@ -306,9 +392,6 @@ ExecStop=/bin/kill -9 $MAINPID
 WantedBy=multi-user.target" > snort3.service
 sleep 2
 fi
-cd /root/
-sleep 2
-rm -R *
 sleep 2
 systemctl daemon-reload
 sleep 12
@@ -319,8 +402,8 @@ sleep 2
 # Auto-Upgrades form Us like bugs fixses and more
 echo "#!/bin/bash
 if [ `id -u` -ne 0 ]; then
-	echo Need sudo
-	exit 1
+        echo Need sudo
+        exit 1
 fi
 
 set -v
@@ -376,16 +459,16 @@ sleep 2
 # Comment for knowlegd your system version and IPv4 address to managed
 sleep 2
 echo "################################################################################
-                                FriendlyWAF 23.12
-        Welcome to our software FriendlyWAF this is a Free version
+                                FriendlyWAF 24.1
+    Welcome to our software FriendlyWAF, this is a Enterprise Version for FREE.
 
-                Web-Panel: http://\4/
+                Proxy: http://\4:81
+                Monitoring: http://\4:19999
                 SSH: \4
 
 ################################################################################
 " > /etc/issue
 sleep 2
-shutdown -r +2
 clear
 
 ######################################################################################
@@ -396,4 +479,5 @@ clear
 #                                                                                    #
 ######################################################################################
 ######################################################################################
-sleep 1
+sleep 5
+reboot
