3f8062c6224b6d801134b902ab5afb83e0d6c1c5
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2023-12-29 14:52:31 +0100

    Add files via upload

---
+++ Upgrade.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# this command give the script sudo perms
+if [ `id -u` -ne 0 ]; then
+        echo Need sudo
+        exit 1
+fi
+
+set -v
+sleep 12
+clear
+
+######################################################################################
+######################################################################################
+#                        FriendlyWAF upgrade 23.12 => 24.1                           #
+#        ----------------------------------------------------------------------      #
+#                                 by FriendlyWAF                                     #
+#                                                                                    #
+######################################################################################
+######################################################################################
+sleep 3
+systemctl stop snort3
+sleep 2
+systemctl stop snort3-nic.service
+sleep 3
+systemctl disable snort3
+sleep 2
+systemctl disable snort3-nic.service
+sleep 3
+cd /root/
+# Snort Upgrade snort3-3.1.76 => snort3-3.1.77.0
+wget http://mirror.friendlywaf.com/Scripts-CE/snort3-3.1.77.0.zip
+sleep 2
+unzip snort3-3.1.77.0.zip
+sleep 2
+cd snort3-3.1.77.0
+sleep 2
+sudo chmod 755 -R *
+sleep 2
+./configure_cmake.sh --prefix=/usr/local --enable-tcmalloc
+sleep 2
+cd build
+sleep 2
+chmod 755 -R *
+sleep 2
+make
+sleep 2
+make install
+sleep 1
+ldconfig
+sleep 1
+snort -V
+sleep 5
+# Removing Apache panel to make it light whight
+rm -R /var/www/html/
+sleep 2
+apt purge apache2 -y
+sleep 2
+# Comment for knowlegd your system version and IPv4 address to managed
+sleep 2
+echo "################################################################################
+                                FriendlyWAF 24.1
+    Welcome to our software FriendlyWAF, this is a Enterprise Version for FREE.
+
+                Proxy: http://\4:81
+                Monitoring: http://\4:19999
+                SSH: \4
+
+################################################################################
+" > /etc/issue
+sleep 5
+clear
+
+######################################################################################
+######################################################################################
+#                  FriendlyWAF upgrade 23.12 => 24.1 has been done                   #
+#        ----------------------------------------------------------------------      #
+#                                 by FriendlyWAF                                     #
+#                                                                                    #
+######################################################################################
+######################################################################################
+sleep 5
+reboot
