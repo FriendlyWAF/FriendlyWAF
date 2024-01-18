ae5aa196d2622218debf867cb21c25379675715d
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2024-01-18 17:41:36 +0100

    Update Upgrade.sh

--- Upgrade.sh
+++ Upgrade.sh
@@ -11,67 +11,9 @@ clear
 
 ######################################################################################
 ######################################################################################
-#                         FriendlyWAF upgrade 24.1 => 24.2                           #
+#                           FriendlyWAF upgrade no need                              #
 #        ----------------------------------------------------------------------      #
 #                                 by FriendlyWAF                                     #
 #                                                                                    #
 ######################################################################################
 ######################################################################################
-sleep 3
-systemctl stop snort3
-sleep 2
-systemctl stop snort3-nic.service
-sleep 3
-cd /root/
-# Snort Upgrade snort3-3.1.76 => snort3-3.1.77.0
-wget http://mirror.friendlywaf.com/Scripts-CE/snort3-3.1.77.0.zip
-sleep 2
-unzip snort3-3.1.77.0.zip
-sleep 2
-cd snort3-3.1.77.0
-sleep 2
-sudo chmod 755 -R *
-sleep 2
-./configure_cmake.sh --prefix=/usr/local --enable-tcmalloc
-sleep 2
-cd build
-sleep 2
-chmod 755 -R *
-sleep 2
-make
-sleep 2
-make install
-sleep 1
-ldconfig
-sleep 1
-snort -V
-sleep 2
-cd /root/
-sleep 1
-rm -R *
-sleep 1
-# Comment for knowlegd your system version and IPv4 address to managed
-sleep 2
-echo "################################################################################
-                                FriendlyWAF 24.2
-    Welcome to our software FriendlyWAF, this is a Enterprise Version for FREE.
-
-                Proxy: http://\4:81
-                Monitoring: http://\4:19999
-                SSH: \4
-
-################################################################################
-" > /etc/issue
-sleep 5
-clear
-
-######################################################################################
-######################################################################################
-#                  FriendlyWAF upgrade 24.1 => 24.2 has been done                   #
-#        ----------------------------------------------------------------------      #
-#                                 by FriendlyWAF                                     #
-#                                                                                    #
-######################################################################################
-######################################################################################
-sleep 5
-reboot
