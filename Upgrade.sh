d9c8fea7e1f63c191009d76c856dded7ea78ee1f
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2024-01-30 10:20:26 +0100

    Update Upgrade.sh

--- Upgrade.sh
+++ Upgrade.sh
@@ -11,7 +11,42 @@ clear
 
 ######################################################################################
 ######################################################################################
-#                           FriendlyWAF upgrade no need                              #
+#                           FriendlyWAF upgrade 24.1 -> 24.2                         #
+#        ----------------------------------------------------------------------      #
+#                                 by FriendlyWAF                                     #
+#                                                                                    #
+######################################################################################
+######################################################################################
+sleep 2
+cd /root/
+# Custom Config made by Us of Snort ++
+wget http://mirror.friendlywaf.com/Scripts-CE/snort3-3.1.78.0.zip
+sleep 2
+unzip snort3-3.1.78.0.zip
+sleep 2
+cd snort3-3.1.78.0
+sleep 2
+sudo chmod 755 -R *
+sleep 2
+./configure_cmake.sh --prefix=/usr/local --enable-tcmalloc
+sleep 2
+cd build
+sleep 2
+chmod 755 -R *
+sleep 2
+make
+sleep 2
+make install
+sleep 1
+ldconfig
+sleep 1
+snort -V
+sleep 5
+clear
+
+######################################################################################
+######################################################################################
+#                           FriendlyWAF upgrade Done :)                              #
 #        ----------------------------------------------------------------------      #
 #                                 by FriendlyWAF                                     #
 #                                                                                    #
