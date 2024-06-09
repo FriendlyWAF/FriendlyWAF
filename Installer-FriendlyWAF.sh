3cf31a7214626241f6c486ce96560e1fdd42a0c5
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2024-06-09 23:09:02 +0200

    Update Installer-FriendlyWAF.sh

--- Installer-FriendlyWAF.sh
+++ Installer-FriendlyWAF.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-# Check if the script has sudo permissions
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Need sudo"
-    exit 1
-fi
-
-set -v
 sleep 5
 
 ######################################################################################
